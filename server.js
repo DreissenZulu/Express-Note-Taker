d1071014bae8738a3258bf282bfd5955f28bc326
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-02 16:22:17 -0400

    Fixed wild card route for index page

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/", function(req, res) {
+app.get("/*", function(req, res) {
     res.sendFile(path.join(mainDir, "index.html"));
 });
 
